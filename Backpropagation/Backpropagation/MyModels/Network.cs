6b1712d7be52ec9864c3aa8b305cd952598921af
ucLh <luchwork@mail.ru>
2018-03-15 20:51:39 +0300

    Minor changes

---
+++ Backpropagation/Backpropagation/MyModels/Network.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkFreshStart
+{
+    public class Network : INetwork
+    {
+        public int InputSize { get; }
+        
+
+        public void Configure(int inputSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Predict(int[] inputVec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Train(int[] trainingVec, int expectedOutput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
