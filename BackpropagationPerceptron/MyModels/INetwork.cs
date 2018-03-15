6f21a69f2f450e4e2eadae8f6a3c589d3e7684bc
ucLh <luchwork@mail.ru>
2018-03-15 20:35:30 +0300

    Added network models

---
+++ BackpropagationPerceptron/MyModels/INetwork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkFreshStart
+{
+    /// <summary>
+    ///   Neural network inferface.
+    /// </summary>
+    public interface INetwork
+    {
+        /// <summary>
+        ///   Number of neurons in input layer.
+        /// </summary>
+        int InputSize { get; }
+
+        /// <summary>
+        ///   Train network using training vector and expected output value.
+        /// </summary>
+        /// <param name="trainingVec">Training vector.</param>
+        /// <param name="expectedOutput">Expected output value.</param>
+        void Train(int[] trainingVec, int expectedOutput);
+
+        /// <summary>
+        ///   Get a predicted value from network using input vector.
+        /// </summary>
+        /// <param name="inputVec">Input vector.</param>
+        /// <returns>Predicted value.</returns>
+        int Predict(int[] inputVec);
+
+        /// <summary>
+        ///   Configure network to have specified number of neurons in input layer.
+        /// </summary>
+        /// <param name="inputSize">Number of neurons in input layer.</param>
+        void Configure(int inputSize);
+    }
+}
